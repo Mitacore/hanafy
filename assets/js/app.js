c9d4ce4fda09c935572d715431774bf8f36f1129
Hanafy <51703150+Mitacore@users.noreply.github.com>
2026-09-17 14:16:44 +0300

    Load full-width layout fix after end section

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,5 +15,6 @@
     .then(() => load('assets/js/viewer.js?v=20260917a'))
     .then(() => load('assets/js/viewer-floating.js?v=20260917a'))
     .then(() => load('assets/js/end-section.js?v=20260917m'))
+    .then(() => load('assets/js/layout-fix.js?v=20260917a'))
     .catch(err => console.error('Portfolio runtime failed to load', err));
 })();

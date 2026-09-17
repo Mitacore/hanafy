bc5acd502aa4e777eb38f03ef4f25e0c79ae4162
Hanafy <51703150+Mitacore@users.noreply.github.com>
2026-09-17 13:08:20 +0300

    Load client logos before end section

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,6 +14,7 @@
     .then(() => load('assets/js/viewer-performance.js?v=20260917a'))
     .then(() => load('assets/js/viewer.js?v=20260917a'))
     .then(() => load('assets/js/viewer-floating.js?v=20260917a'))
-    .then(() => load('assets/js/end-section.js?v=20260917g'))
+    .then(() => load('assets/js/client-wall.js?v=20260917a'))
+    .then(() => load('assets/js/end-section.js?v=20260917h'))
     .catch(err => console.error('Portfolio runtime failed to load', err));
 })();

15a72d560285b3ada13b67b469a39f627a208c19
Hanafy <51703150+Mitacore@users.noreply.github.com>
2026-09-17 14:30:05 +0300

    Refresh About layout adjustments

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,7 +15,7 @@
     .then(() => load('assets/js/viewer.js?v=20260917a'))
     .then(() => load('assets/js/viewer-floating.js?v=20260917a'))
     .then(() => load('assets/js/end-section.js?v=20260917m'))
-    .then(() => load('assets/js/layout-fix.js?v=20260917c'))
+    .then(() => load('assets/js/layout-fix.js?v=20260917d'))
     .then(() => load('assets/js/content-scale-fix.js?v=20260917a'))
     .catch(err => console.error('Portfolio runtime failed to load', err));
 })();
